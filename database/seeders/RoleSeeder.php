3b2ad42b747566c6ecff7d435bb6e70445276e4d
brandonvera <brandonvera@valdusoft.com>
2022-09-20 07:58:56 -0400

    Insersion de roles con seeder

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class RoleSeeder extends Seeder
 {
@@ -14,6 +15,34 @@ class RoleSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('roles')->insert([
+        	'name' => 'Admin',
+        	"created_at" => date('Y-m-d H:m:s'),
+            "updated_at" => date('Y-m-d H:m:s'),
+        ]);
+
+        DB::table('roles')->insert([
+        	'name' => 'Financiero',
+        	"created_at" => date('Y-m-d H:m:s'),
+            "updated_at" => date('Y-m-d H:m:s'),
+        ]);
+
+        DB::table('roles')->insert([
+        	'name' => 'Contable',
+        	"created_at" => date('Y-m-d H:m:s'),
+            "updated_at" => date('Y-m-d H:m:s'),
+        ]);
+
+        DB::table('roles')->insert([
+        	'name' => 'Vendedor',
+        	"created_at" => date('Y-m-d H:m:s'),
+            "updated_at" => date('Y-m-d H:m:s'),
+        ]);
+
+        DB::table('roles')->insert([
+        	'name' => 'Usuario',
+        	"created_at" => date('Y-m-d H:m:s'),
+            "updated_at" => date('Y-m-d H:m:s'),
+        ]);
     }
 }
