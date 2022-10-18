be7522d72c73aa79b862b46703db7669ecd3b83f
Cesar Montevideo <77177023+cesar899@users.noreply.github.com>
2022-10-18 12:10:40 -0400

    cambios

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -18,11 +18,11 @@ class OrderController extends Controller
     }
 
     public function store (StoreRequest $request){
-        // $userRepo = new UserRepository();
-        $orderData = $request->validated();
+         // $userRepo = new UserRepository();
+        //$orderData = $request->validated();
         // $newUser = $userRepo->create($request->safe()->only(['name','last_name', 'dni', 'address','phone_number']));
 
-        if($request->amount !== 0){
+        /*if($request->amount !== 0){
 
             $sale = new Sale();
             $sale->user_id = $request->user_id;
@@ -42,6 +42,42 @@ class OrderController extends Controller
             $orderData['voucher'] = $request->voucher;
         }
         $order = $this->repository->create($orderData);
+
+        return response()->json($order , 200);*/
+
+
+        if($request->amount !== 0){
+
+            $sale = new Sale();
+            $sale->user_id = $request->user_id;
+            $sale->seller_id = 2;
+            $sale->batch_id = 2;
+            $sale->status = 0;
+            $sale->amount = 1000;
+            $sale->amount_paid = 200;
+            $sale->cuote_account = 5;
+            $sale->cuote_paid = 1;
+            $sale->paid->comming = 5;
+            $sale->day_payment = Carbon::now();
+            $sale->save();
+
+            $order = new Order();
+            $order->bank_name = $request->bank_name;
+            $order->user_id = $request->user_id;
+            $order->sale_id = $sale->id;
+            $order->seller_id = $request->seller_id;
+            $order->ref_number = $request->ref_number;
+            $order->status = 0;
+            $order->voucher = $request->voucher;
+
+             //Guardamos el voucher
+            $voucher  = $request->file('voucher');
+            $name = time() . "." . $voucher ->extension();
+            $voucher->move(public_path('storage') . '/voucher/', $name);
+            $order->payment_proof = '' . $name;
+
+        }
+        //$orderNew = $this->repository->create($order);
 
         return response()->json($order , 200);
     }
--- app/Repositories/Orders/OrderRepository.php
+++ app/Repositories/Orders/OrderRepository.php
@@ -11,7 +11,7 @@ class OrderRepository
 	public function create($data) {
 
 		$path = Storage::disk('public')->put('payment_proof', $data['voucher']);
-		$data['payment_proof'] = $path ?? null; 
+		$data['payment_proof'] = $path ?? null;
 		$path = $data['voucher']->move( public_path("images/payment_proof/"), $path);
 		$deleted = Storage::deleteDirectory("/public/payment_proof");
 
